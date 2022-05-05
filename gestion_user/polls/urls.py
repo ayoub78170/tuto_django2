1677f0ee8fa8098cc57c695e3b5871eca0349a3c
ayoub78170 <ayoub.assiali78@gmail.com>
2022-05-05 11:21:07 +0200

    init projet

--- gestion_user/polls/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from . import views
-# /polls/
-urlpatterns = [
-    path('', views.index ,name ='index'),
-    path('<int:user_id>/', views.detail , name='detail'),
-]
